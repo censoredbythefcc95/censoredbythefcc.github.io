33598abf1fa0899e6beb3f0dc3ef26944a99a527
Your Name <sastudillo6@gmail.com>
2021-01-13 08:42:35 -0500

    working on hamburger JS

--- js/app.js
+++ js/app.js
@@ -5,10 +5,10 @@
 $.ajax("https://spreadsheets.google.com/feeds/list/17l-upBkEU85B21P8i8AzLKyBHMDS7vDdLsDvt2EVcIk/1/public/full?alt=json")
 .then((data) => {
     // Checking my Data
-    console.log(data)
+    console.log(data);
 
     //put our projects in a variable
-const rawProjects = data.feed.entry
+const rawProjects = data.feed.entry;
 
 //log our projects
 console.log(rawProjects)
@@ -24,7 +24,27 @@ const projects = rawProjects.map((project) => {
     }
 })
 
-console.log(projects)
-})
+console.log(projects);
 
+//Mobile Navigation Action
+let menuOpen = false;
+$(".mobilenavbuttons").click(() => {
+  if (menuOpen == false) {
+    $(".displayoverlay).css("visibility", "visible");
+    $(".displayoverlay").css("opacity", "1");
+    $(".displayoverlay").css("transition", "all 0.75s ease-in-out");
+    $(".displayoverlay").css("height", "100%");
+    $(".mainmenuButton").addClass("open");
+    menuOpen = true;
+  } else {
+    $(".displayoverlay").css("visibility", "hidden");
+    $(".displayoverlay").css("opacity", "0");
+    $(".displayoverlay").css("transition", "all 0.5s ease-in-out");
+    $(".displayoverlay").css("height", "0");
+    $(".mainmenuButton").removeClass("open");
+    menuOpen = false;
+    }
+});
+
+//not functional, must work on
     
